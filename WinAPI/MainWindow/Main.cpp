87ceacb7d4d76e618bdd51c107e588530e8632b4
D4STEW143 <dastew143@mail.ru>
2024-11-19 20:16:01 +0900

    Homework done

--- WinAPI/MainWindow/Main.cpp
+++ WinAPI/MainWindow/Main.cpp
@@ -1,6 +1,10 @@
-﻿#include <Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include "resource.h"
+#include<cstdio>
 
 CONST char g_sz_WINDOW_CLASS[] = "Main Window";
+CONST char WinName[] = "%s. Size: %ix%i; Corners Location:LT:(%ix%i),  LB:(%ix%i),  RT:(%ix%i),  RB:(%ix%i)";
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -16,9 +20,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR  lpCmdLine, I
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));
+	wClass.hCursor = (HCURSOR)LoadImage(hInstance, "Cursors\\Hand.ani", IMAGE_CURSOR, 32, 32, LR_LOADFROMFILE);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.hInstance = hInstance;
@@ -39,8 +43,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR  lpCmdLine, I
 			g_sz_WINDOW_CLASS,					//Class name
 			g_sz_WINDOW_CLASS,					//Window title
 			WS_OVERLAPPEDWINDOW,				//Window style. Такой стиль всегда задется для главного окна. 
-			CW_USEDEFAULT, CW_USEDEFAULT,		//Position - положение окна при запуске
-			CW_USEDEFAULT, CW_USEDEFAULT,		//Size - размер создаваемого окна
+			(GetSystemMetrics(SM_CXSCREEN) * 12) / 100, (GetSystemMetrics(SM_CYSCREEN) * 12) / 100,		//Position - положение окна при запуске
+			(GetSystemMetrics(SM_CXSCREEN) * 75) / 100, (GetSystemMetrics(SM_CYSCREEN) * 75) / 100,		//Size - размер создаваемого окна
 			NULL,								//Parent
 			NULL,								//hMenu: 
 												//для главного окна это ResourceID главного меню. 
@@ -76,6 +80,51 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		break;
+	case WM_SIZE:
+	{
+		CHAR change[512]{};
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT width = rect.right - rect.left;
+		INT height = rect.bottom - rect.top;
+		sprintf
+		(
+			change,
+			WinName,
+			g_sz_WINDOW_CLASS,
+			width,
+			height,
+			(int)rect.left, (int)rect.top,
+			(int)rect.left, (int)rect.bottom,
+			(int)rect.right, (int)rect.top,
+			(int)rect.right, (int)rect.bottom
+		);
+		SetWindowText(hwnd, change);
+	}
+	break;
+	case WM_MOVE:
+	{
+		CHAR change[512]{};
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT width = rect.right - rect.left;
+		INT height = rect.bottom - rect.top;
+		//sprintf_s(locChng, "%s (Location LT:(%ix%i),  LB:(%ix%i),  RT:(%ix%i),  RB:(%ix%i)", g_sz_WINDOW_CLASS, (int)rect.left, (int)rect.top, (int)rect.left, (int)rect.bottom, (int)rect.right, (int)rect.top, (int)rect.right, (int)rect.bottom);
+		sprintf
+		(
+			change,
+			WinName,
+			g_sz_WINDOW_CLASS,
+			width,
+			height,
+			(int)rect.left, (int)rect.top,
+			(int)rect.left, (int)rect.bottom,
+			(int)rect.right, (int)rect.top,
+			(int)rect.right, (int)rect.bottom
+		);
+		SetWindowText(hwnd, change);
+	}
+	break;
 	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
