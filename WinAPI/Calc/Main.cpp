e34b62e473f6c80ab094be7703d257d76deb3fbb
D4STEW143 <dastew143@mail.ru>
2024-12-01 17:50:17 +0900

    Logic left undone.

--- WinAPI/Calc/Main.cpp
+++ WinAPI/Calc/Main.cpp
@@ -1,4 +1,7 @@
-﻿#include <Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <cstdio>
+#include "resource.h"
 
 #define IDC_EDIT_DISPLAY		999
 
@@ -38,6 +41,8 @@ CONST INT g_i_START_Y = 10;
 CONST INT g_i_BUTTON_START_X = g_i_START_X;
 CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_SCREEN_HEIGHT + g_i_INTERVAL;
 
+CONST INT sz_SIZE = 256;
+
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -51,8 +56,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;
 
-	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 	wClass.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
@@ -298,8 +303,216 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDC_BUTTON_0:
 		{
-			MessageBox(hwnd, "0", "0", MB_OK);
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _0 = '0';
+			CHAR sz_buf0_0[sz_SIZE]{};
+			CHAR sz_buf0_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf0_0);
+			sprintf(sz_buf0_1, "%s%c", sz_buf0_0, _0);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf0_1);
 		}
+		break;
+		case IDC_BUTTON_1:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _1 = '1';
+			CHAR sz_buf1_0[sz_SIZE]{};
+			CHAR sz_buf1_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf1_0);
+			sprintf(sz_buf1_1, "%s%c", sz_buf1_0, _1);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf1_1);
+		}
+		break;
+		case IDC_BUTTON_2:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _2 = '2';
+			CHAR sz_buf2_0[sz_SIZE]{};
+			CHAR sz_buf2_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf2_0);
+			sprintf(sz_buf2_1, "%s%c", sz_buf2_0, _2);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf2_1);
+		}
+		break;
+		case IDC_BUTTON_3:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _3 = '3';
+			CHAR sz_buf3_0[sz_SIZE]{};
+			CHAR sz_buf3_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf3_0);
+			sprintf(sz_buf3_1, "%s%c", sz_buf3_0, _3);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf3_1);
+		}
+		break;
+		case IDC_BUTTON_4:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _4 = '4';
+			CHAR sz_buf4_0[sz_SIZE]{};
+			CHAR sz_buf4_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf4_0);
+			sprintf(sz_buf4_1, "%s%c", sz_buf4_0, _4);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf4_1);
+		}
+		break;
+		case IDC_BUTTON_5:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _5 = '5';
+			CHAR sz_buf5_0[sz_SIZE]{};
+			CHAR sz_buf5_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf5_0);
+			sprintf(sz_buf5_1, "%s%c", sz_buf5_0, _5);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf5_1);
+		}
+		break;
+		case IDC_BUTTON_6:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _6 = '6';
+			CHAR sz_buf6_0[sz_SIZE]{};
+			CHAR sz_buf6_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf6_0);
+			sprintf(sz_buf6_1, "%s%c", sz_buf6_0, _6);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf6_1);
+		}
+		break;
+		case IDC_BUTTON_7:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _7 = '7';
+			CHAR sz_buf7_0[sz_SIZE]{};
+			CHAR sz_buf7_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf7_0);
+			sprintf(sz_buf7_1, "%s%c", sz_buf7_0, _7);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf7_1);
+		}
+		break;
+		case IDC_BUTTON_8:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _8 = '8';
+			CHAR sz_buf8_0[sz_SIZE]{};
+			CHAR sz_buf8_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf8_0);
+			sprintf(sz_buf8_1, "%s%c", sz_buf8_0, _8);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf8_1);
+		}
+		break;
+		case IDC_BUTTON_9:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _9 = '9';
+			CHAR sz_buf9_0[sz_SIZE]{};
+			CHAR sz_buf9_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf9_0);
+			sprintf(sz_buf9_1, "%s%c", sz_buf9_0, _9);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf9_1);
+		}
+		break;
+		case IDC_BUTTON_POINT:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _point = '.';
+			CHAR sz_buf_point_0[sz_SIZE]{};
+			CHAR sz_buf_point_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf_point_0);
+			if (sz_buf_point_0[SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) - 1] == _point)
+			{
+				MessageBox(hwnd, "Same symbol", "Error", MB_OK | MB_ICONERROR);
+			}
+			else
+			{
+				sprintf(sz_buf_point_1, "%s%c", sz_buf_point_0, _point);
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf_point_1);
+			}
+		}
+		break;
+		case IDC_BUTTON_PLUS:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _plus = '+';
+			CHAR sz_buf_plus_0[sz_SIZE]{};
+			CHAR sz_buf_plus_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf_plus_0);
+			if (sz_buf_plus_0[SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) - 1] == _plus)
+			{
+				MessageBox(hwnd, "Same symbol", "Error", MB_OK | MB_ICONERROR);
+			}
+			else
+			{
+				sprintf(sz_buf_plus_1, "%s%c", sz_buf_plus_0, _plus);
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf_plus_1);
+			}
+		}
+		break;
+
+		// TODO:
+		// ПЕРЕПИШИ НАЗВАНИЯ МАСИВОВ ПОД ЕДИНЫМ ИМЕНЕМ sz_buf_0 & sz_buf_1
+		//
+
+		case IDC_BUTTON_MINUS:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _minus = '-';
+			CHAR sz_buf_0[sz_SIZE]{};
+			CHAR sz_buf_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf_0);
+			if (sz_buf_0[SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) - 1] == _minus)
+			{
+				MessageBox(hwnd, "Same symbol", "Error", MB_OK | MB_ICONERROR);
+			}
+			else
+			{
+				sprintf(sz_buf_1, "%s%c", sz_buf_0, _minus);
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf_1);
+			}
+		}
+		break;
+		case IDC_BUTTON_ASTER:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _aster = '*';
+			CHAR sz_buf_0[sz_SIZE]{};
+			CHAR sz_buf_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf_0);
+			if (sz_buf_0[SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) - 1] == _aster)
+			{
+				MessageBox(hwnd, "Same symbol", "Error", MB_OK | MB_ICONERROR);
+			}
+			else
+			{
+				sprintf(sz_buf_1, "%s%c", sz_buf_0, _aster);
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf_1);
+			}
+		}
+		break;
+		case IDC_BUTTON_SLASH:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR _slash = '/';
+			CHAR sz_buf_0[sz_SIZE]{};
+			CHAR sz_buf_1[sz_SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, sz_SIZE, (LPARAM)sz_buf_0);
+			if (sz_buf_0[SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) - 1] == _slash)
+			{
+				MessageBox(hwnd, "Same symbol", "Error", MB_OK | MB_ICONERROR);
+			}
+			else
+			{
+				sprintf(sz_buf_1, "%s%c", sz_buf_0, _slash);
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buf_1);
+			}
+		}
+		break;
+		case IDC_BUTTON_BSP:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
+			CHAR sz_buf_0[sz_SIZE];
+			//CHAR sz_buf_1[sz_SIZE];
+		}
+		break;
 		break;
 		}
 #endif // LOGIC
