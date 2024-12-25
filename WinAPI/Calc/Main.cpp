3f9cbf29711c9c7aee7af2a4529859c4e30df583
D4STEW143 <dastew143@mail.ru>
2024-12-25 15:39:05 +0900

    LoadFontsFromDLL function done;

--- WinAPI/Calc/Main.cpp
+++ WinAPI/Calc/Main.cpp
@@ -4,6 +4,7 @@
 #include "resource.h"
 #include "Dimentions.h"
 #include "Skins.h"
+#include "Fonts.h"
 #include<cstdio>
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc_VPD_311";
@@ -17,6 +18,8 @@ INT GetTitleBarHeight(HWND hwnd);
 VOID SetSkin(HWND hwnd, CONST CHAR skin[]);
 VOID SetFonts(HWND hwnd, CONST CHAR fonts[]);
 VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[]);
+VOID LoadFontFromDLL(HMODULE hFontModule, INT resourceID);
+VOID LoadFontsFromDLL(HMODULE hFontModule);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -94,13 +97,14 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		);
 
 		hFontsModule = LoadLibrary("Fonts.dll");
-		HRSRC hFntRes = FindResource(hFontsModule, MAKEINTRESOURCE(2002), MAKEINTRESOURCE(RT_FONT));
+		/*HRSRC hFntRes = FindResource(hFontsModule, MAKEINTRESOURCE(2003), MAKEINTRESOURCE(RT_FONT));
 		HGLOBAL hFntMem = LoadResource(hFontsModule, hFntRes);
 		VOID* fntData = LockResource(hFntMem);
 		DWORD nFonts = 0;
 		DWORD len = SizeofResource(hFontsModule, hFntRes);
 		AddFontMemResourceEx(fntData, len, NULL, &nFonts);
-		//AddFontResource("Fonts\\MOSCOW2024.otf");
+		AddFontResource("Fonts\\MOSCOW2024.otf");*/
+		LoadFontsFromDLL(hFontsModule);
 		HFONT hFont = CreateFont
 		(
 			g_i_FONT_HEIGHT, g_i_FONT_WIDTH,
@@ -111,7 +115,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CLIP_CHARACTER_PRECIS,
 			ANTIALIASED_QUALITY,
 			FF_DONTCARE,
-			"Terminator Two"
+			g_FONT_NAMES[0]
 		);
 
 		SendMessage(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
@@ -489,17 +493,26 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		//1)создаем всплывающее меню
 		HMENU hMenu = CreatePopupMenu();
+		HMENU hMenuSkins = CreatePopupMenu();
+		HMENU hMenuFonts = CreatePopupMenu();
 
 		//2)добавляем пункты в это меню
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_MOSCOW_2024, "Moscow 2024");
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_TERMINATOR, "Terminator");
+		InsertMenu(hMenuFonts, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_DIGITAL_7, "Digital 7");
+
+
+		InsertMenu(hMenuSkins, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_METAL_MISTRAL, "Metal Mistral");
+		InsertMenu(hMenuSkins, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_SQUARE_BLUE, "Square Blue");
+
 		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING, IDR_EXIT, "Exit");
 		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_SEPARATOR, 0, NULL);
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_FONT_THECAPT, "The Capt");
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_FONT_MOSCOW, "Moscow2024");
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_SEPARATOR, 0, NULL);
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_METAL_MISTRAL, "Metal Mistral");
-		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING | MF_UNCHECKED, IDR_SQUARE_BLUE, "Square Blue");
-		CheckMenuItem(hMenu, index, MF_BYPOSITION | MF_CHECKED);
-		CheckMenuItem(hMenu, font_index + 3, MF_BYPOSITION | MF_CHECKED);
+		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_POPUP, (UINT_PTR)hMenuFonts, "Fonts");
+		InsertMenu(hMenu, 0, MF_BYPOSITION | MF_POPUP, (UINT_PTR)hMenuSkins, "Skins");
+
+		//CheckMenuItem(hMenu, index, MF_BYPOSITION | MF_CHECKED);
+		CheckMenuItem(hMenuSkins, index, MF_BYPOSITION | MF_CHECKED);
+		//CheckMenuItem(hMenuFonts, font_index, MF_BYPOSITION | MF_CHECKED);
 
 		//3)Открыть меню
 		DWORD item = TrackPopupMenu(hMenu, TPM_RIGHTALIGN | TPM_BOTTOMALIGN | TPM_RETURNCMD, LOWORD(lParam), HIWORD(lParam), 0, hwnd, NULL);
@@ -508,10 +521,6 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDR_SQUARE_BLUE:	//index = IDR_SQUARE_BLUE;		break;
 		case IDR_METAL_MISTRAL:	//SetSkin(hwnd, "metal_mistral");		break;
 			index = item - IDR_SQUARE_BLUE;
-			break;
-		case IDR_FONT_MOSCOW:
-		case IDR_FONT_THECAPT:
-			font_index = item - IDR_FONT_MOSCOW;
 			break;
 		case IDR_EXIT: SendMessage(hwnd, WM_CLOSE, 0, 0);	break;
 		}
@@ -527,6 +536,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_DESTROY:
 		FreeLibrary(hFontsModule);
+		
 		PostQuitMessage(0);
 		break;
 	case WM_CLOSE:
@@ -651,4 +661,22 @@ VOID SetSkinFromDLL(HWND hwnd, CONST CHAR skin[])
 		//IMAGE_BITMAP можно не указывать, а оставить NULL
 	}
 	FreeLibrary(hModule);
+}
+
+VOID LoadFontFromDLL(HMODULE hFontModule, INT resourceID)
+{
+	HRSRC hFntRes = FindResource(hFontModule, MAKEINTRESOURCE(resourceID), MAKEINTRESOURCE(RT_FONT));
+	HGLOBAL hFntMem = LoadResource(hFontModule, hFntRes);
+	VOID* fntData = LockResource(hFntMem);
+	DWORD nFonts = 0;
+	DWORD len = SizeofResource(hFontModule, hFntRes);
+	AddFontMemResourceEx(fntData, len, NULL, &nFonts);
+}
+
+VOID LoadFontsFromDLL(HMODULE hFontModule)
+{
+	for (int i = 2001; i <= 2003; i++)
+	{
+		LoadFontFromDLL(hFontModule, i);
+	}
 }
