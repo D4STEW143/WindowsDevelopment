ff5b04f2c7b972c16cdbd584cd803e66e1a1ca20
D4STEW143 <dastew143@mail.ru>
2024-12-29 21:42:28 +0900

    TODO: HideControls button.

---
+++ WindowsForms/Clock/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			//labelTime.Text = DateTime.Now.ToString("hh:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+			labelTime.Text = DateTime.Now.ToString("HH:mm:ss");
+			if(checkBoxShowDate.Checked )
+			{
+				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+			}
+			if (checkBoxShowWeekDay.Checked)
+			{
+				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+			}
+		}
+
+		private void buttonHideControls_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
